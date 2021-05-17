e2db81188a7fabeff466a12fca4590e92a0f7c7c
Karolina Bagrowska <karolina.bagrowska@gmail.com>
2021-05-17 23:13:51 +0200

    5.4. Change schemas class SupplierPut.

--- schemas.py
+++ schemas.py
@@ -72,14 +72,14 @@ class SupplierNew(BaseModel):
 
 class SupplierPut(BaseModel):
     CompanyName: constr(max_length=40)
-    ContactName: constr(max_length=30) = None
-    ContactTitle: constr(max_length=30) = None
-    Address: constr(max_length=60) = None
-    City: constr(max_length=15) = None
-    Region: constr(max_length=15) = None
-    PostalCode: constr(max_length=10) = None
-    Country: constr(max_length=15) = None
-    Phone: constr(max_length=24) = None
+    ContactName: constr(max_length=30)
+    ContactTitle: constr(max_length=30)
+    Address: constr(max_length=60)
+    City: constr(max_length=15)
+    Region: constr(max_length=15)
+    PostalCode: constr(max_length=10)
+    Country: constr(max_length=15)
+    Phone: constr(max_length=24)
     Fax: constr(max_length=24) = None
     HomePage: Text = None
 
